13f68d881b77c678b1d62b378d2817aa141c01e3
long040903 <129354994+long040903@users.noreply.github.com>
2023-08-06 14:15:48 +0700

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import logo from "./logo.svg";
 import "./App.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from "./header/Header";
 import Footer from "./footer/Footer";
 import React from "react";
-import { BrowserRouter as Router, Routes, Route
-,Link  } from "react-router-dom";
+import {
+  BrowserRouter as Router, Routes, Route
+  , Link
+} from "react-router-dom";
 import Login from "./Login";
+import Parent from "./Parents";
+import Parents from "./Parents";
 
 function App() {
   return (
@@ -20,7 +25,17 @@ function App() {
             <Route path="/login" element={<Login />} />{" "}
             {/* Add a route for the Login component */}
           </Routes>
+          <Link to="/Parents">
+            <button>parent</button>
+          </Link>
+
+
+          <Routes>
+            <Route path="/Parents" element={<Parents/>} />{" "}
+            {/* Add a route for the Login component */}
+          </Routes>
         </div>
+
         <Footer />
       </div>
     </Router>
