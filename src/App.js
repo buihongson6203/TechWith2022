51bcdaf58f8381658edef8be26acfcead76aca7e
Thanh Huyen <phamthanhhuyen19124>
2023-08-07 12:35:15 +0700

    update file home

--- src/App.js
+++ src/App.js
@@ -3,8 +3,7 @@ import "./App.css";
 import Header from "./header/Header";
 import Footer from "./footer/Footer";
 import React from "react";
-import { BrowserRouter as Router, Routes, Route
-,Link  } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import Login from "./Login";
 
 function App() {
@@ -14,7 +13,56 @@ function App() {
         <Header />
         <Link to="/login">
           <button>Login 123</button>
+          <div></div>
         </Link>
+        <div>
+          <div className="welcome">Welcome to Debug Portal</div>
+          <input className="input" type="text" placeholder="name student..." />
+          {/* <form className="radio">
+            <input name="gender" type="radio" value="Nam" />
+            I am Student
+            <input name="gender" type="radio" value="Nữ" />
+            I am Teacher
+            <input name="gender" type="radio" value="Khác" />
+            I am Parent/Guardian
+          </form> */}
+          <div class="d-flex justify-content-center">
+            <div class="form-check">
+              <input
+                class="form-check-input lp-input-radio ng-pristine ng-untouched ng-valid ng-empty"
+                type="radio"
+                ng-model="type"
+                ng-value="1"
+                name="3"
+                value="1"
+              />
+              <label class="form-check-label">I am Student</label>
+            </div>
+            <div class="form-check">
+              <input
+                class="form-check-input lp-input-radio ng-pristine ng-untouched ng-valid ng-empty"
+                type="radio"
+                ng-model="type"
+                ng-value="1"
+                name="3"
+                value="1"
+              />
+              <label class="form-check-label">I am Teacher</label>
+            </div>
+            <div class="form-check">
+              <input
+                class="form-check-input lp-input-radio ng-pristine ng-untouched ng-valid ng-empty"
+                type="radio"
+                ng-model="type"
+                ng-value="1"
+                name="3"
+                value="1"
+              />
+              <label class="form-check-label">I am Parent/Guardian</label>
+            </div>
+          </div>
+          <button className="btn">Access</button>
+        </div>
         <div>
           <Routes>
             <Route path="/login" element={<Login />} />{" "}
